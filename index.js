6c0bac6e495f03e18b068520d1c47759a08ad79e
Ethan Tiller <tilleres@mail.uc.edu>
2025-03-03 23:09:53 -0500

    pray

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 document.addEventListener('DOMContentLoaded', (event) => {
+    // Initialize Auth0 with environment variables
     const auth0 = new Auth0Client({
         domain: AUTH0_DOMAIN,
         client_id: AUTH0_CLIENT_ID,
         redirect_uri: AUTH0_REDIRECT_URI
     });
-  
+
     const loginButton = document.getElementById('loginButton');
     const signinButton = document.getElementById('signinButton');
-  
+
     loginButton.addEventListener('click', async () => {
         try {
             await auth0.loginWithRedirect();
@@ -15,7 +16,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
             console.error('Error logging in:', error);
         }
     });
-  
+
     signinButton.addEventListener('click', async () => {
         try {
             await auth0.loginWithRedirect();
@@ -23,7 +24,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
             console.error('Error logging in:', error);
         }
     });
-  
+
     const handleRedirectCallback = async () => {
         try {
             await auth0.handleRedirectCallback();
@@ -32,8 +33,8 @@ document.addEventListener('DOMContentLoaded', (event) => {
             console.error('Error handling redirect callback:', error);
         }
     };
-  
+
     if (window.location.search.includes('code=')) {
         handleRedirectCallback();
     }
-  });
+});
